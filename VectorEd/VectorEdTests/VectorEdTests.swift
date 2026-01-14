a6a9e9f8d00d03d5df2a9a673936ff02043b602b
Serhii Dunets <dunets.deve@gmail.com>
2026-01-14 10:17:53 +0000

    Add empty project

---
+++ VectorEd/VectorEdTests/VectorEdTests.swift
@@ -0,0 +1,17 @@
+//
+//  VectorEdTests.swift
+//  VectorEdTests
+//
+//  Created by alf on 14.01.2026.
+//
+
+import Testing
+@testable import VectorEd
+
+struct VectorEdTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
